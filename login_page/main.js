b7efc813c60d0d2f1e31bd5923a1a096e37be075
vutuankien <vutuankien030904@gmail.com>
2024-06-24 20:47:29 +0700

    push to main

--- login_page/main.js
+++ login_page/main.js
@@ -2,15 +2,15 @@ function signup(){
     const email = document.getElementById('register-email').value;
     const password = document.getElementById('register-password').value;
     if (email === '' || password === '') {
-        swal("Vui lòng nhập đầy đủ thông tin !");
+        alert('Vui lòng nhập đầy đủ thông tin.');
         return;
     }
     if (localStorage.getItem(email)) {
-        swal("Tên đăng nhập đã tồn tại !");
+        alert('Tên đăng nhập đã tồn tại.');
         return;
     }
     localStorage.setItem(email, password);
-    swal("Đăng ký thành công","", "Success");
+    alert('Đăng kí thành công!');
 
     document.getElementById('register-container').style.display = 'none';
     document.getElementById('login-container').style.display = 'block';
@@ -22,26 +22,22 @@ function login(){
     const password = document.getElementById('login-password').value;
 
     if (email === '' || password === '') {
-        swal("Vui lòng nhập đầy đủ thông tin !");
+        alert('Vui lòng nhập đầy đủ thông tin.');
         return;
     }
 
     const storedPassword = localStorage.getItem(email);
 
     if (storedPassword === null) {
-        swal("Tên đăng nhập không tồn tại!");
+        alert('Tên đăng nhập không tồn tại.');
         return;
     }
 
     if (storedPassword !== password) {
-        swal('Mật khẩu không đúng.');
+        alert('Mật khẩu không đúng.');
         return;
     }
-    swal('Đăng nhập thành công !', "Chúc bạn có 1 trải nghiệm vui vẻ","Success");
-    setTimeout(() => {
-        window.location.href = '/index.html';
-    }, 3000);
-
-    console.log("say hello if logged in");
-
+    alert('Dang nhap thanh cong');
+    var newUrl = window.location.protocol + "//" + window.location.host + "/index.html";
+    window.history.replaceState({ path: newUrl }, '', newUrl);
 }
